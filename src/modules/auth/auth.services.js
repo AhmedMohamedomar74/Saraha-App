d3b2f15764a11ef26df5226e0e9b42a938e7250a
Ahmed Mohamed Omar <ahmedmohamedomar74@gmail.com>
2025-08-10 23:28:51 +0300

    add hash with bycrypt

--- src/modules/auth/auth.services.js
+++ src/modules/auth/auth.services.js
@@ -1,5 +1,6 @@
 import { failedResponse ,successResponce } from "../services.js"
 import userModel from "./../../DB/models/User.model.js"
+import bcrypt  from "bcryptjs"
 export const signup = async (req, res, next) => {
     try {
 
@@ -21,6 +22,11 @@ export const signup = async (req, res, next) => {
          * @error 
          * the phone number save as null and same to email
          */
+        const {password} =  req.body
+        let n_password = bcrypt.hashSync(password,10)
+        req.body.password = n_password
+        // console.log({n_password})
+        // console.log(req.body)
         const newUser = new userModel(req.body)
         await newUser.save()
         console.log(newUser.age)
